16c73e837eca44208ee1701412655aa79b85cb52
WillieMaddox <willie.maddox@gmail.com>
2018-03-01 17:46:41 -0600

    Implement WaveProperty class.

--- discrimnn/signal.py
+++ discrimnn/signal.py
@@ -1,48 +1,57 @@
 import numpy as np
 
 
+class WaveProperty:
+    def __init__(self, kwargs):
+        assert 'mean' in kwargs
+        self.mean = kwargs['mean']
+        self.delta = kwargs['delta'] if 'delta' in kwargs else 0
+        self._value = None
+
+    @property
+    def value(self):
+        if self._value is None:
+            self._generate()
+        return self._value
+
+    def generate(self):
+        self._generate()
+        return self._value
+
+    def _generate(self):
+        self._value = self.mean if self.delta == 0 else (2 * np.random.random() - 1) * self.delta + self.mean
+
+
 class Signal:
     def __init__(self,
                  timestamps,
-                 offset=0,
-                 offset_range=None,
-                 amplitude=1,
-                 amplitude_range=None,
-                 period=1,
-                 period_range=None,
-                 phase=0,
-                 phase_range=None):
+                 offset=None,
+                 amplitude=None,
+                 period=None,
+                 phase=None):
 
         self.timestamps = timestamps
         self._sample = None
 
-        if offset_range is not None:
-            assert len(offset_range) == 2
-        else:
-            assert offset is not None
-        self.offset = offset
-        self.offset_range = offset_range
+        offset = {} if offset is None else offset
+        if 'mean' not in offset:
+            offset['mean'] = 0
+        self.offset = WaveProperty(offset)
 
-        if amplitude_range is not None:
-            assert len(amplitude_range) == 2
-        else:
-            assert amplitude is not None
-        self.amplitude = amplitude
-        self.amplitude_range = amplitude_range
+        amplitude = {} if amplitude is None else amplitude
+        if 'mean' not in amplitude:
+            amplitude['mean'] = 1
+        self.amplitude = WaveProperty(amplitude)
 
-        if period_range is not None:
-            assert len(period_range) == 2
-        else:
-            assert period is not None
-        self.period = period
-        self.period_range = period_range
+        period = {} if period is None else period
+        if 'mean' not in period:
+            period['mean'] = 1
+        self.period = WaveProperty(period)
 
-        if phase_range is not None:
-            assert len(phase_range) == 2
-        else:
-            assert phase is not None
-        self.phase = phase
-        self.phase_range = phase_range
+        phase = {} if phase is None else phase
+        if 'mean' not in phase:
+            phase['mean'] = 0
+        self.phase = WaveProperty(phase)
 
     def __call__(self):
         return self.sample
@@ -53,27 +62,23 @@ class Signal:
             self._sample = self.generate()
         return self._sample
 
-    @staticmethod
-    def _sample_random(r, r_range):
-        return r if r_range is None else (r_range[1] - r_range[0]) * np.random.random() + r_range[0]
+    def generate(self, offset=None, amplitude=None, period=None, phase=None):
 
-    def generate(self):
+        offset = self.offset.generate() if offset is None else offset
+        amplitude = self.amplitude.generate() if amplitude is None else amplitude
+        period = self.period.generate() if period is None else period
+        phase = self.phase.generate() if phase is None else phase
 
-        self.offset = self._sample_random(self.offset, self.offset_range)
-        self.amplitude = self._sample_random(self.amplitude, self.amplitude_range)
-        self.period = self._sample_random(self.period, self.period_range)
-        self.phase = self._sample_random(self.phase, self.phase_range)
-
-        self._sample = self.offset + self.amplitude * np.cos(2.0 * np.pi * self.timestamps / self.period - self.phase)
+        self._sample = offset + amplitude * np.cos(2.0 * np.pi * self.timestamps / period - phase)
         return self._sample
 
 
 class MixedSignal:
     def __init__(self, start_time, stop_time, n_timesteps, sig_coeffs):
         self._signals = None
-        self._inputs = None
-        self._labels = None
-        self._one_hots = None
+        self.inputs = None
+        self.labels = None
+        self.one_hots = None
         self.mixed_signal = None
         self.n_signals = len(sig_coeffs)
         self.n_timesteps = n_timesteps
@@ -85,6 +90,8 @@ class MixedSignal:
             self.signal_objects.append(Signal(self.timestamps, **coeffs))
 
     def __call__(self):
+        if self.inputs is None or self.one_hots is None:
+            self.generate()
         return self.inputs, self.one_hots
 
     def __len__(self):
@@ -98,47 +105,31 @@ class MixedSignal:
                 self._signals[i, :] = signal()
         return self._signals
 
-    @property
-    def inputs(self):
-        if self._inputs is None:
-            self.generate()
-        return self._inputs
-
-    @property
-    def labels(self):
-        if self._labels is None:
-            self.generate()
-        return self._labels
-
-    @property
-    def one_hots(self):
-        if self._one_hots is None:
-            self.generate()
-        return self._one_hots
-
     def generate(self):
         shuffled_indexes = np.arange(self.n_timesteps)
         np.random.shuffle(shuffled_indexes)
-        self._labels = np.zeros(self.n_timesteps, dtype=int)
+        self.labels = np.zeros(self.n_timesteps, dtype=int)
         for s in range(self.n_signals):
-            self._labels[np.where(shuffled_indexes < s * self.n_timesteps // self.n_signals)] += 1
+            self.labels[np.where(shuffled_indexes < s * self.n_timesteps // self.n_signals)] += 1
 
-        self._one_hots = np.zeros((self.n_timesteps, self.n_signals), dtype=float)
-        labels_tup = (np.arange(self.n_timesteps), self._labels)
-        self._one_hots[labels_tup] = 1
+        self.one_hots = np.zeros((self.n_timesteps, self.n_signals), dtype=float)
+        labels_tup = (np.arange(self.n_timesteps), self.labels)
+        self.one_hots[labels_tup] = 1
 
         self._signals = None
         for signal in self.signal_objects:
             signal.generate()
-        self.mixed_signal = np.sum(self._one_hots.T * self.signals, axis=0)
-        # self._inputs = np.vstack((self.timestamps, self.mixed_signal)).T
+        self.mixed_signal = np.sum(self.one_hots.T * self.signals, axis=0)
+        self.inputs = np.vstack((self.timestamps, self.mixed_signal)).T
 
         # self._inputs = x.reshape(self.n_timesteps, 2, 1)
-        self._inputs = self.mixed_signal.reshape(self.n_timesteps, 1, 1)
+        self.inputs = self.inputs.reshape(self.n_timesteps, 1, 2)
         # self._inputs = x.reshape(1, self.n_timesteps, 2)
         # self._inputs = x.reshape(self.n_timesteps, 2)
 
-        return self._inputs, self._one_hots
+        # self._inputs = self.mixed_signal.reshape(self.n_timesteps, 1, 1)
+
+        return self.inputs, self.one_hots
 
     def generate_batch(self, batch_size):
         x_batch = np.empty((batch_size, *self.inputs.shape))
