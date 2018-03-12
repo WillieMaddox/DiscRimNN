a8b38948a894fafe78eb35e862f1a7970b4e374d
WillieMaddox <willie.maddox@gmail.com>
2018-03-11 21:01:59 -0500

    Add name and color generator functions.

--- discrimnn/signal.py
+++ discrimnn/signal.py
@@ -1,4 +1,15 @@
+import string
 import numpy as np
+
+
+def name_generator():
+    alphabet = np.array(list(string.ascii_uppercase))
+    return ''.join(alphabet[np.random.randint(0, len(alphabet), 3)])
+
+
+def color_generator():
+    r, g, b = np.random.randint(0, 255, 3)
+    return '#%02X%02X%02X' % (r, g, b)
 
 
 class WaveProperty:
