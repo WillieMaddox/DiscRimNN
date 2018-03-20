ba83c40fcdb5c9e6a953b2d848bb80a308eb9d66
WillieMaddox <willie.maddox@gmail.com>
2018-03-20 09:42:01 -0500

    Add OUNoise tests

--- mixsig/noise.py
+++ mixsig/noise.py
@@ -30,14 +30,30 @@ class NormalNoise:
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
 
-    def __init__(self, n_timestamps, n_signals=None, mu=0.0, theta=0.15, sigma=0.2):
+    def __init__(self, n_signals=1, n_timestamps=None, mu=None, theta=0.15, sigma=0.2):
         """Initialize parameters and noise process."""
-        self.n_timestamps = n_timestamps
         self.n_signals = n_signals
-        self.mu = mu
+        self.n_timestamps = n_timestamps
+
+        if isinstance(mu, np.ndarray):
+            assert len(mu) == n_signals
+            self.mu = mu
+        elif isinstance(mu, int):
+            self.mu = np.ones(self.n_signals) * float(mu)
+        elif isinstance(mu, float):
+            self.mu = np.ones(self.n_signals) * mu
+        elif mu is None:
+            self.mu = np.zeros(self.n_signals)
+        else:
+            raise ValueError('What is mu???')
+
+        # self.mu = mu if mu is not None else np.zeros(self.n_signals)
+
         self.theta = theta
         self.sigma = sigma
-        self.state = mu
+
+        self.state = np.ones(self.n_signals) * self.mu
+
         self._signals = None
 
     def __call__(self):
@@ -49,21 +65,27 @@ class OUNoise:
             self._signals = self.generate()
         return self._signals
 
-    def generate(self, n_signals=1):
-        signals = np.empty((n_signals, self.n_timestamps))
+    def generate(self, n_timestamps=None, reset=False):
+        if n_timestamps is not None:
+            self.n_timestamps = n_timestamps
+        if self.n_timestamps is None:
+            raise ValueError('Timestamps are None')
+        if reset:
+            self.reset()
+
+        self._signals = np.empty((self.n_signals, self.n_timestamps))
         for i in range(self.n_timestamps):
-            signals[:, i] = self.sample()
-        self._signals = signals
+            self._signals[:, i] = self.sample()
         return self._signals
 
     def sample(self):
         """Update internal state and return it as a noise sample."""
         x = self.state
-        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn()
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
         self.state = x + dx
         return self.state
 
     def reset(self):
         """Reset the internal state (= noise) to mean (mu)."""
-        self.state = self.mu
+        self.state = np.ones(self.n_signals) * self.mu
 
---
+++ tests/test_noise.py
@@ -0,0 +1,93 @@
+import pytest
+from hypothesis import given
+from hypothesis import example
+from hypothesis import strategies as st
+from hypothesis.extra.numpy import arrays
+import numpy as np
+from mixsig.noise import OUNoise
+
+
+st_ounoise_kwargs = st.fixed_dictionaries(
+    dict(
+        n_signals=st.integers(min_value=1, max_value=20),
+        # n_timestamps=st.one_of(
+        #     st.none(),
+        #     st.integers(min_value=2, max_value=20000)
+        # ),
+        n_timestamps=st.integers(min_value=2, max_value=20000),
+        mu=st.one_of(
+            st.none(),
+            st.floats(min_value=0.0, max_value=1.0, allow_nan=False),
+            # arrays(float, ())
+        ),
+        theta=st.floats(min_value=0.0, max_value=5.0, allow_nan=False),
+        sigma=st.floats(min_value=0.0, max_value=5.0, allow_nan=False)
+    )
+)
+
+
+@given(st_ounoise_kwargs)
+@example(kwargs={'n_signals': 1, 'n_timestamps': 30, 'mu': 0.0})
+@example(kwargs={'n_signals': 2, 'n_timestamps': 30, 'mu': 0.0})
+def test_ounoise_inputs(kwargs):
+    noise = OUNoise(**kwargs)
+    assert noise().shape == (noise.n_signals, noise.n_timestamps)
+    assert noise.signals.shape == (noise.n_signals, noise.n_timestamps)
+
+
+@given(st_ounoise_kwargs)
+@example(kwargs={'n_signals': 1, 'n_timestamps': 30, 'mu': None})
+@example(kwargs={'n_signals': 2, 'n_timestamps': 30, 'mu': 0})
+@example(kwargs={'n_signals': 10, 'n_timestamps': 30, 'mu': 5})
+@example(kwargs={'n_signals': 1, 'n_timestamps': 30, 'mu': np.random.randn(1)})
+@example(kwargs={'n_signals': 2, 'n_timestamps': 30, 'mu': np.random.randn(2)})
+@example(kwargs={'n_signals': 10, 'n_timestamps': 30, 'mu': np.random.randn(10)})
+def test_ounoise_mu_as_numpy_array(kwargs):
+    noise = OUNoise(**kwargs)
+    assert np.all(noise.state == noise.mu)
+    noise.generate(n_timestamps=50)
+    if noise.sigma > 0:
+        assert np.all(noise.state != noise.mu)
+    else:
+        assert np.all(noise.state == noise.mu)
+    noise.reset()
+    assert np.all(noise.state == noise.mu)
+
+
+ounoise = st.builds(
+    OUNoise,
+    n_signals=st.integers(min_value=1, max_value=20),
+    n_timestamps=st.integers(min_value=2, max_value=20000),
+    mu=st.one_of(
+        st.none(),
+        st.floats(min_value=0.0, max_value=1.0, allow_nan=False),
+        # arrays(float, ())
+    ),
+    theta=st.floats(min_value=0.0, max_value=5.0, allow_nan=False),
+    sigma=st.floats(min_value=0.0, max_value=5.0, allow_nan=False)
+)
+
+
+@given(ounoise)
+@example(OUNoise(n_signals=1, n_timestamps=30, mu=0.0))
+@example(OUNoise(n_signals=2, n_timestamps=30, mu=np.random.randn(2)))
+@example(OUNoise(n_signals=5, n_timestamps=30, mu=0.0))
+def test_ounoise_generator(noise):
+    assert noise.signals.shape == (noise.n_signals, noise.n_timestamps)
+    noise.generate(n_timestamps=50)
+    assert noise.signals.shape == (noise.n_signals, noise.n_timestamps)
+
+
+@given(st_ounoise_kwargs)
+@example(kwargs={'n_signals': 1, 'n_timestamps': 30, 'mu': 0.0})
+@example(kwargs={'n_signals': 2, 'n_timestamps': 30, 'mu': 0.0})
+def test_ounoise_reset(kwargs):
+    noise = OUNoise(**kwargs)
+    assert np.all(noise.state == noise.mu)
+    noise.generate(n_timestamps=50, reset=True)
+    if noise.sigma > 0:
+        assert np.all(noise.state != noise.mu)
+    else:
+        assert np.all(noise.state == noise.mu)
+    noise.reset()
+    assert np.all(noise.state == noise.mu)
