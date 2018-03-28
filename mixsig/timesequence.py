0b3b05464b1fbcd0bdd085076145c77425cb7985
WillieMaddox <willie.maddox@gmail.com>
2018-03-27 20:13:22 -0500

    Change TimeSequence properties start, stop, and n_timestamps to hard args.

--- mixsig/timesequence.py
+++ mixsig/timesequence.py
@@ -22,38 +22,6 @@ class TimeSequence:
     def __call__(self):
         return self.timestamps
 
-    # @property
-    # def start(self):
-    #     if self._start is None:
-    #         self._start = self._stop - (self._n_timestamps - 1) * self._dt
-    #     return self._start
-
-    # @property
-    # def stop(self):
-    #     if self._stop is None:
-    #         self._stop = self._start + (self._n_timestamps - 1) * self._dt
-    #     return self._stop
-
-    # @property
-    # def n_timestamps(self):
-    #     if self._n_timestamps is None:
-    #         n = (self._stop - self._start) / self._dt
-    #         assert np.abs(n - np.rint(n)) <= np.finfo(float).eps
-    #         self._n_timestamps = 1 + n
-    #     return self._n_timestamps
-
-    # @property
-    # def dt(self):
-    #     if self._dt is None:
-    #         self._dt = (self.stop - self.start) / (self.n_timestamps - 1)
-    #     return self._dt
-
-    # @property
-    # def uniform_timestamps(self):
-    #     if self._uniform_timestamps is None:
-    #         self._uniform_timestamps = np.linspace(self.start, self.stop, self.n_timestamps)
-    #     return self._uniform_timestamps
-
     @property
     def timestamps(self):
         if self._timestamps is None:
