dc9d6ed3fd99152eb0e7e7b5b99e43d646d89ab1
WillieMaddox <willie.maddox@gmail.com>
2018-03-27 19:35:25 -0500

    Move name and color generators into new utils.py module.

--- mixsig/waves.py
+++ mixsig/waves.py
@@ -1,15 +1,7 @@
 import string
 import numpy as np
-
-
-def name_generator():
-    alphabet = np.array(list(string.ascii_uppercase))
-    return ''.join(alphabet[np.random.randint(0, len(alphabet), 3)])
-
-
-def color_generator():
-    r, g, b = np.random.randint(0, 255, 3)
-    return '#%02X%02X%02X' % (r, g, b)
+from .utils import name_generator
+from .utils import color_generator
 
 
 class WaveProperty:
