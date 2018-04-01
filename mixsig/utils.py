1de6a97abc16b0b14a5259bf9ebc91ef6e6bc677
WillieMaddox <willie.maddox@gmail.com>
2018-04-01 01:14:47 -0500

    add typing for returns in utils generators

--- mixsig/utils.py
+++ mixsig/utils.py
@@ -1,13 +1,14 @@
 import string
 import numpy as np
+from typing import Text
 
 
-def name_generator():
+def name_generator() -> Text:
     alphabet = np.array(list(string.ascii_uppercase))
     return ''.join(alphabet[np.random.randint(0, len(alphabet), 3)])
 
 
-def color_generator():
+def color_generator() -> Text:
     r, g, b = np.random.randint(0, 255, 3)
     return '#%02X%02X%02X' % (r, g, b)
 
