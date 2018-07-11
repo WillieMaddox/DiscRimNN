f7f879cd2aaf285ae3af48ca564484c71db6f87a
WillieMaddox <willie.maddox@gmail.com>
2018-07-11 09:08:04 -0500

    Add sequence_code attribute to MixedSignal class.
    With this, sequence_type is now a property that is defined by the sequence_code.
    
    Update the sliding and boxcar generators to use the sequence_code.
    
    Allow sequence_code to be passed into generators for to facilitate unit testing.
    
    Because of this, the generate() function is now in charge of setting the X and y variables.
    
    Remove the useless _generate() function and merge its contents into generator()
    
    Add full coverage unit tests for sliding and boxcar generators.

--- mixsig/mixed.py
+++ mixsig/mixed.py
@@ -22,7 +22,7 @@ class MixedSignal:
                  window_size=1,
                  window_type='sliding',
                  network_type='RNN',
-                 sequence_type='many2one',
+                 sequence_code='t_tc',
                  run_label=None,
                  name='Mixed',
                  n_groups=5):
@@ -42,15 +42,16 @@ class MixedSignal:
         self.batch_size = batch_size
         self.window_size = window_size
         self.network_type = network_type
-        self.sequence_type = sequence_type
+        self.sequence_code = sequence_code
+        self._sequence_type = None
         self.name = name
 
         if window_type is not None:
             window_type = window_type.lower()
             assert window_type in ('sliding', 'boxcar')
         self.window_type = window_type
-        assert self.network_type in ('MLP', 'RNN')
-        assert self.sequence_type in ('many2one', 'many2many', 'many2one+time')
+        assert self.network_type in ('MLP', 'RNN', 'LSTM', 'PLSTM', 'TCN')
+        assert self.sequence_code in ('t_t', 't_tc', 't1_tc', 'xw_xc', 'xw1_xc', 'xw_xwc', 'xw1_xwc')
 
         if 'time' in msig_coeffs:
             self.sequence_generator = timesequence_generator(**msig_coeffs['time'])
@@ -81,10 +82,11 @@ class MixedSignal:
 
         self.config_dict = {
             'run_label': run_label,
-            'window_size': window_size,
-            'window_type': window_type,
-            'network_type': network_type,
-            'sequence_type': sequence_type,
+            'window_size': self.window_size,
+            'window_type': self.window_type,
+            'network_type': self.network_type,
+            'sequence_code': self.sequence_code,
+            'sequence_type': self.sequence_type,
             'sigs_coeffs': sigs_coeffs,
             'msig_coeffs': msig_coeffs,
         }
@@ -98,6 +100,21 @@ class MixedSignal:
         self.config_filename = os.path.join(self.out_dir, 'mixed_signal_config.json')
         self.model_weights_filename = os.path.join(self.out_dir, 'model_weights.h5')
         self.training_stats_filename = os.path.join(self.out_dir, 'training_stats.csv')
+
+    @property
+    def sequence_type(self):
+        if self._sequence_type is None:
+            if self.sequence_code in ('t_t', 't_tc', 't1_tc'):
+                self._sequence_type = 'one2one'
+            elif self.sequence_code in ('xw_xc', 'xw1_xc'):
+                self._sequence_type = 'many2one'
+            elif self.sequence_code in ('xw_xwc', 'xw1_xwc'):
+                self._sequence_type = 'many2many'
+                if self.network_type in ('PLSTM', 'PhasedLSTM'):
+                    self._sequence_type = self._sequence_type + '+time'
+            else:
+                raise ValueError('Invalid sequence_code')
+        return self._sequence_type
 
     def _generate_signals_old(self):
         """ Generate waves from property values."""
@@ -144,18 +161,20 @@ class MixedSignal:
         # Sanity check
         assert len(timestamps) == len(mixed_signal) == len(labels)
 
-        # Now we want to sort the labels and mixed_signal chronologically.
+        # Sort the labels and mixed_signal chronologically.
         sorted_indices = np.argsort(timestamps)
 
         # clip data from the left so that it divisible by batch_size.
         if self.window_type == 'sliding':
             chop_index = (len(timestamps) - self.window_size + 1) % self.batch_size
         elif self.window_type == 'boxcar':
-            assert len(timestamps) >= self.window_size
-            fact_a = factors(self.batch_size)
-            fact_b = factors(self.window_size)
-            gcm = max(fact_a.intersection(fact_b))
-            chop_index = len(timestamps) % (self.window_size * self.batch_size // gcm)
+            # assert len(timestamps) >= self.window_size
+            # fact_a = factors(self.batch_size)
+            # fact_b = factors(self.window_size)
+            # gcm = max(fact_a.intersection(fact_b))
+            # chop_index = len(timestamps) % (self.window_size * self.batch_size // gcm)
+            assert len(timestamps) >= self.window_size * self.batch_size
+            chop_index = len(timestamps) % (self.window_size * self.batch_size)
         else:
             chop_index = len(timestamps) % self.batch_size
 
@@ -180,93 +199,144 @@ class MixedSignal:
 
         assert self.n_samples % self.batch_size == 0
 
-    def _generate(self):
+    def generate(self):
         self._generate_signals()
         self.one_hots = create_one_hots_from_labels(self.labels, self.n_classes)
+        # window_type, window_size
 
-    def generate(self):
-        self._generate()
+        # sliding, ws < 1               ->  raise ValueError('Please specify a window_size')
+        # sliding, ws = 1               ->  valid same as (boxcar, 1)
+        # sliding, 1 < ws < n_timesteps ->  valid
+        # sliding, n_timesteps = ws     ->  valid same as (None, 0), (None, 1)
+        # sliding, n_timesteps < ws     ->  raise ValueError('window_size must be <= n_timestamps')
+
+        # boxcar, ws < 1               ->  raise ValueError('Please specify a window_size')
+        # boxcar, ws = 1               ->  valid same as (sliding, 1)
+        # boxcar, 1 < ws < n_timesteps ->  valid
+        # boxcar, n_timesteps = ws     ->  valid same as (None, 0), (None, 1)
+        # boxcar, n_timesteps < ws     ->  raise ValueError('window_size must be <= n_timestamps')
+
+        # random, ws < 1               ->  raise ValueError('Please specify a window_size')
+        # random, ws = 1               ->  raise NotImplementedError
+        # random, 1 < ws < n_timesteps ->  valid
+        # random, n_timesteps = ws     ->  valid same as (None, 0), (None, 1)
+        # random, n_timesteps < ws     ->  raise ValueError('window_size must be <= n_timestamps')
+
+        # None,    < 1  ->  valid
+        # None,    = 1  ->  valid
+        # None,    > 1  ->  raise ValueError('Please specify a window_size')
+
         if self.window_type == 'sliding':
-            self.generate_sliding()
+            self.X, self.y = self.generate_sliding()
         elif self.window_type == 'boxcar':
-            self.generate_boxcar()
+            self.X, self.y = self.generate_boxcar()
         elif self.window_type is None:
-            self.generate_sliding()
+            self.X, self.y = self.generate_sliding()
         else:
-            raise ValueError('improper window_type: {}. Use "sliding" or "boxcar" or None')
+            raise ValueError('Invalid window_type: {}. Use "sliding", "boxcar" or None')
         return self.X, self.y
 
-    def generate_sliding(self):
+    def generate_sliding(self, sequence_code=None):
 
         # TODO: unit tests to make sure all these pathways are correct.
-        if self.network_type == 'MLP':
-            if self.sequence_type == 'many2one':
-                # MLP: many to one
-                self.X = np.zeros((self.n_samples, self.window_size))
-                for i in range(self.window_size):
-                    self.X[:, i] = self.mixed_signal[i:i + self.n_samples]
-                self.y = self.one_hots[(self.window_size - 1):]
-            elif self.sequence_type == 'many2many':
-                # MLP: many to many (1088, 100, 1) (1088, 100, 3)
-                self.X = np.zeros((self.n_samples, self.window_size, 1))
-                self.y = np.zeros((self.n_samples, self.window_size, self.n_classes))
-                for i in range(self.window_size):
-                    self.X[:, i, 0] = self.mixed_signal[i:i + self.n_samples]
-                    self.y[:, i] = self.one_hots[i:i + self.n_samples]
-            else:
-                raise NotImplementedError
-        else:
-            if self.sequence_type == 'many2one':
-                # RNN: many to one (1088, 100, 1) (1088, 3)
-                self.X = np.zeros((self.n_samples, self.window_size, 1))
-                for i in range(self.window_size):
-                    self.X[:, i, 0] = self.mixed_signal[i:i + self.n_samples]
-                self.y = self.one_hots[(self.window_size - 1):]
-            elif self.sequence_type == 'many2one+time':
-                # RNN: many to one (1088, 100, 2) (1088, 3)
-                self.X = np.zeros((self.n_samples, self.window_size, 2))
-                for i in range(self.window_size):
-                    self.X[:, i, 0] = self.mixed_signal[i:i + self.n_samples]
-                    self.X[:, i, 1] = self.timestamps[i + self.n_samples - 1] - self.timestamps[i:i + self.n_samples]
-                self.y = self.one_hots[(self.window_size - 1):]
-            elif self.sequence_type == 'many2many':
-                # RNN: many to many (1088, 100, 1) (1088, 100, 3)
-                self.X = np.zeros((self.n_samples, self.window_size, 1))
-                self.y = np.zeros((self.n_samples, self.window_size, self.n_classes))
-                for i in range(self.window_size):
-                    self.X[:, i, 0] = self.mixed_signal[i:i + self.n_samples]
-                    self.y[:, i] = self.one_hots[i:i + self.n_samples]
-            else:
-                raise NotImplementedError
+        # sequence_types
+        # t -> n_[t]imestamps
+        # x -> n_samples (or number of sub-samples)
+        # w -> [w]indow_size
+        # c -> n_[c]lasses
+        # one2one   t_t     (1200,)        (1200,)
+        # one2one   t_tc    (1200,)        (1200, 3)
+        # one2one   t1_tc   (1200, 1)      (1200, 3)
+        # many2one  xw_xc   (1088, 100)    (1088, 3)
+        # many2one  xw1_xc  (1088, 100, 1) (1088, 3)
+        # many2many xw_xwc  (1088, 100)    (1088, 100, 3)
+        # many2many xw1_xwc (1088, 100, 1) (1088, 100, 3)
 
-    def generate_boxcar(self):
+        if self.sequence_type == 'many2one+time':
+            # PLSTM: many2one (1088, 100, 2) -> (1088, 3)
+            X = np.zeros((self.n_samples, self.window_size, 2))
+            for i in range(self.window_size):
+                X[:, i, 0] = self.mixed_signal[i:i + self.n_samples]
+                X[:, i, 1] = self.timestamps[i + self.n_samples - 1] - self.timestamps[i:i + self.n_samples]
+            y = self.one_hots[(self.window_size - 1):]
+            return X, y
 
-        if self.network_type == 'MLP':
-            if self.sequence_type == 'many2one':
-                # MLP: many to one
-                self.X = self.mixed_signal.reshape((self.n_samples, self.window_size))
-                labels = self.one_hots.reshape((self.n_samples, self.window_size, self.n_classes))
-                labels = labels[:, -1, :]
-                self.y = labels.reshape(self.n_samples, self.n_classes)
-            elif self.sequence_type == 'many2many':
-                # MLP: many to many
-                self.X = self.mixed_signal.reshape((self.n_samples, self.window_size, 1))
-                self.y = self.one_hots.reshape((self.n_samples, self.window_size, self.n_classes))
-            else:
-                raise NotImplementedError
+        sequence_code = sequence_code or self.sequence_code
+
+        X_code, y_code = sequence_code.split('_')
+        if X_code == 't':
+            X = self.mixed_signal
+        elif X_code == 't1':
+            X = self.mixed_signal.reshape((self.n_timestamps, 1))
+        elif X_code == 'xw':
+            X = np.zeros((self.n_samples, self.window_size))
+            for i in range(self.window_size):
+                X[:, i] = self.mixed_signal[i:i + self.n_samples]
+        elif X_code == 'xw1':
+            X = np.zeros((self.n_samples, self.window_size, 1))
+            for i in range(self.window_size):
+                X[:, i, 0] = self.mixed_signal[i:i + self.n_samples]
         else:
-            if self.sequence_type == 'many2one':
-                # RNN: many to one
-                self.X = self.mixed_signal.reshape((self.n_samples, self.window_size, 1))
-                labels = self.one_hots.reshape((self.n_samples, self.window_size, self.n_classes))
-                labels = labels[:, -1, :]
-                self.y = labels.reshape(self.n_samples, self.n_classes)
-            elif self.sequence_type == 'many2many':
-                # RNN: many to many
-                self.X = self.mixed_signal.reshape((self.n_samples, self.window_size, 1))
-                self.y = self.one_hots.reshape((self.n_samples, self.window_size, self.n_classes))
-            else:
-                raise NotImplementedError
+            raise NotImplementedError
+
+        if y_code == 't':
+            y = self.labels
+        elif y_code == 'tc':
+            y = self.one_hots
+        elif y_code == 'xc':
+            y = self.one_hots[(self.window_size - 1):]
+        elif y_code == 'xwc':
+            y = np.zeros((self.n_samples, self.window_size, self.n_classes))
+            for i in range(self.window_size):
+                y[:, i] = self.one_hots[i:i + self.n_samples]
+        else:
+            raise NotImplementedError
+
+        return X, y
+
+    def generate_boxcar(self, sequence_code=None):
+
+        # TODO: Need unit tests to make sure all these pathways are correct.
+        # sequence_types
+        # x -> n_samples (or sub-samples)
+        # w -> [w]indow_size
+        # c -> n_[c]lasses
+        # one2one   t_t     (1088,)        (1088,)
+        # one2one   t_tc    (1088,)        (1088, 3)
+        # one2one   t1_tc   (1088, 1)      (1088, 3)
+        # many2one  xw_xc   (1088, 100)    (1088, 3)
+        # many2one  xw1_xc  (1088, 100, 1) (1088, 3)
+        # many2many xw_xwc  (1088, 100)    (1088, 100, 3)
+        # many2many xw1_xwc (1088, 100, 1) (1088, 100, 3)
+
+        sequence_code = sequence_code or self.sequence_code
+        X_code, y_code = sequence_code.split('_')
+
+        if X_code == 't':
+            X = self.mixed_signal
+        elif X_code == 't1':
+            X = self.mixed_signal.reshape((self.n_timestamps, 1))
+        elif X_code == 'xw':
+            X = self.mixed_signal.reshape((self.n_samples, self.window_size))
+        elif X_code == 'xw1':
+            X = self.mixed_signal.reshape((self.n_samples, self.window_size, 1))
+        else:
+            raise NotImplementedError
+
+        if y_code == 't':
+            y = self.labels
+        elif y_code == 'tc':
+            y = self.one_hots
+        elif y_code == 'xc':
+            y = self.one_hots.reshape((self.n_samples, self.window_size, self.n_classes))
+            y = y[:, -1, :]
+            y = y.reshape(self.n_samples, self.n_classes)
+        elif y_code == 'xwc':
+            y = self.one_hots.reshape((self.n_samples, self.window_size, self.n_classes))
+        else:
+            raise NotImplementedError
+
+        return X, y
 
     def generate_group(self, n_msigs, shuffle_inplace=False):
         # examples:
--- tests/test_mixed.py
+++ tests/test_mixed.py
@@ -52,9 +52,9 @@ def test_create_from_3_waves_0_noise():
         sigs_coeffs,
         msig_coeffs=msig_coeffs,
         batch_size=batch_size,
-
         window_size=window_size,
         window_type='sliding',
+        sequence_code='xw1_xc',
         run_label='test'
     )
 
@@ -117,6 +117,7 @@ def test_create_from_2_waves_1_noise():
         batch_size=batch_size,
         window_size=window_size,
         window_type='sliding',
+        sequence_code='xw1_xc',
         run_label='test'
     )
 
@@ -180,6 +181,7 @@ def test_create_from_1_waves_2_noise():
         batch_size=batch_size,
         window_size=window_size,
         window_type='sliding',
+        sequence_code='xw1_xc',
         run_label='test'
     )
 
@@ -198,53 +200,84 @@ def test_create_from_1_waves_2_noise():
     assert np.all([sig.color for sig in msig.waves] == ['#ff0000', '#00ff00', '#0000ff'])
 
 
-def test_create_from_3_waves_boxcar():
-    wave1_coeffs = {
-        'amplitude': {'mean': 1.0, 'delta': 0},
-        'frequency': {'mean': 1, 'delta': 0},
-        'offset': {'mean': -0.1, 'delta': 0},
-        'phase': {'mean': 0, 'delta': 0},
-        'name': 'A',
-        'color': '#ff0000'
-    }
-    wave2_coeffs = {
-        'amplitude': {'mean': 1.0, 'delta': 0},
-        'frequency': {'mean': 1, 'delta': 0},
-        'offset': {'mean': 0.0, 'delta': 0},
-        'phase': {'mean': 0, 'delta': 0},
-        'name': 'B',
-        'color': '#00ff00'
-    }
-    wave3_coeffs = {
-        'amplitude': {'mean': 1.0, 'delta': 0},
-        'frequency': {'mean': 1, 'delta': 0},
-        'offset': {'mean': 0.1, 'delta': 0},
-        'phase': {'mean': 0, 'delta': 0},
-        'name': 'C',
-        'color': '#0000ff'
-    }
-    sigs_coeffs = [wave1_coeffs, wave2_coeffs, wave3_coeffs]
-
-    msig_coeffs = {
-        'amplitude': {'mean': 10, 'delta': 2},
-        'frequency': {'mean': 25, 'delta': 0},
-        'offset': {'mean': 1, 'delta': 5},
-        'phase': {'mean': 0, 'delta': 1},
-        'time': {'t_min': 0, 't_max': 75, 'n_timestamps': 301}
-    }
-
-    batch_size = 7
-    window_size = 9
-
+def test_invalid_window_type():
     with pytest.raises(AssertionError):
         MixedSignal(
-            sigs_coeffs,
-            msig_coeffs=msig_coeffs,
-            batch_size=batch_size,
-            window_size=window_size,
-            window_type='boxcrap',
+            [{'offset': {'mean': -0.1}}],
+            window_type='boxcart',
             run_label='test'
         )
+
+
+def test_generate_sliding():
+    wave1_coeffs = {'offset': {'mean': -0.1}}
+    wave2_coeffs = {'offset': {'mean': 0.0}}
+    wave3_coeffs = {'offset': {'mean': 0.1}}
+    sigs_coeffs = [wave1_coeffs, wave2_coeffs, wave3_coeffs]
+    msig_coeffs = {'time': {'t_min': 0, 't_max': 2, 'n_timestamps': 500}}
+
+    batch_size = 128
+    window_size = 10
+    msig = MixedSignal(
+        sigs_coeffs,
+        msig_coeffs=msig_coeffs,
+        batch_size=batch_size,
+        window_size=window_size,
+        window_type='sliding',
+        sequence_code='xw1_xwc',
+        run_label='test'
+    )
+
+    X, y = msig.generate()
+    n_timestamps = msig.n_timestamps
+    n_labels = len(msig.labels)
+    n_samples = msig.n_samples
+    n_classes = msig.n_classes
+    # code_map = {'x': n_samples, 'w': window_size, 't': n_timestamps, 'c': n_classes}
+    assert msig.n_classes == len(msig.waves) == len(sigs_coeffs)
+    assert len(X) % batch_size == 0
+    assert len(y) % batch_size == 0
+
+    sequence_codes = {
+        't_t': {
+            'x': (n_timestamps, ),
+            'y': (n_labels, )},
+        't_tc': {
+            'x': (n_timestamps, ),
+            'y': (n_timestamps, n_classes)},
+        't1_tc': {
+            'x': (n_timestamps, 1),
+            'y': (n_timestamps, n_classes)},
+        'xw_xc': {
+            'x': (n_samples, window_size),
+            'y': (n_samples, n_classes)},
+        'xw1_xc': {
+            'x': (n_samples, window_size, 1),
+            'y': (n_samples, n_classes)},
+        'xw_xwc': {
+            'x': (n_samples, window_size),
+            'y': (n_samples, window_size, n_classes)},
+        'xw1_xwc': {
+            'x': (n_samples, window_size, 1),
+            'y': (n_samples, window_size, n_classes)}}
+
+    for sequence_code, shapes in sequence_codes.items():
+        X, y = msig.generate_sliding(sequence_code)
+        assert len(X.shape) == len(shapes['x']), print(sequence_code)
+        assert len(y.shape) == len(shapes['y']), print(sequence_code)
+        assert X.shape == shapes['x'], print(sequence_code)
+        assert y.shape == shapes['y'], print(sequence_code)
+
+
+def test_generate_boxcar():
+    wave1_coeffs = {'offset': {'mean': -0.1}}
+    wave2_coeffs = {'offset': {'mean': 0.0}}
+    wave3_coeffs = {'offset': {'mean': 0.1}}
+    sigs_coeffs = [wave1_coeffs, wave2_coeffs, wave3_coeffs]
+    msig_coeffs = {'time': {'t_min': 0, 't_max': 2, 'n_timestamps': 500}}
+
+    batch_size = 1
+    window_size = 100
 
     msig = MixedSignal(
         sigs_coeffs,
@@ -252,22 +285,55 @@ def test_create_from_3_waves_boxcar():
         batch_size=batch_size,
         window_size=window_size,
         window_type='boxcar',
+        sequence_code='xw1_xc',
         run_label='test'
     )
     # msig.save_config()
-    assert len(msig.waves) == len(sigs_coeffs)
     X, y = msig.generate()
+    n_timestamps = msig.n_timestamps
+    n_labels = len(msig.labels)
+    n_samples = msig.n_samples
+    n_classes = msig.n_classes
+    assert msig.n_classes == len(msig.waves) == len(sigs_coeffs)
+
     assert len(X) % batch_size == 0
     assert len(y) % batch_size == 0
-    assert len(msig.X) % batch_size == 0
-    assert len(msig.y) % batch_size == 0
     assert np.all(msig.X == X)
     assert np.all(msig.y == y)
     assert msig.n_timestamps % window_size == 0
-    assert msig.X.shape == (msig.n_timestamps / window_size, window_size, 1)
-    assert msig.y.shape == (msig.n_timestamps / window_size, len(sigs_coeffs))
-    assert np.all([sig.name for sig in msig.waves] == ['A', 'B', 'C'])
-    assert np.all([sig.color for sig in msig.waves] == ['#ff0000', '#00ff00', '#0000ff'])
+    assert msig.X.shape == (msig.n_timestamps // window_size, window_size, 1)
+    assert msig.y.shape == (msig.n_timestamps // window_size, len(sigs_coeffs))
+
+    sequence_codes = {
+        't_t': {
+            'x': (n_timestamps, ),
+            'y': (n_labels, )},
+        't_tc': {
+            'x': (n_timestamps, ),
+            'y': (n_timestamps, n_classes)},
+        't1_tc': {
+            'x': (n_timestamps, 1),
+            'y': (n_timestamps, n_classes)},
+        'xw_xc': {
+            'x': (n_samples, window_size),
+            'y': (n_samples, n_classes)},
+        'xw1_xc': {
+            'x': (n_samples, window_size, 1),
+            'y': (n_samples, n_classes)},
+        'xw_xwc': {
+            'x': (n_samples, window_size),
+            'y': (n_samples, window_size, n_classes)},
+        'xw1_xwc': {
+            'x': (n_samples, window_size, 1),
+            'y': (n_samples, window_size, n_classes)}}
+
+    for sequence_code, shapes in sequence_codes.items():
+        X, y = msig.generate_boxcar(sequence_code)
+        assert len(X.shape) == len(shapes['x']), print(sequence_code)
+        assert len(y.shape) == len(shapes['y']), print(sequence_code)
+        assert X.shape == shapes['x'], print(sequence_code)
+        assert y.shape == shapes['y'], print(sequence_code)
+
 
 
 @fixture
@@ -332,6 +398,7 @@ def test_generate_config(datadir):
         msig_coeffs=msig_coeffs,
         window_size=window_size,
         window_type='sliding',
+        sequence_code='xw1_xc',
         run_label='test'
     )
     truth_filename = datadir('mixed_signal_config.json')
@@ -345,5 +412,3 @@ def test_generate_config(datadir):
     assert signal_config_truth == signal_config_test
 
 
-def test_generate_sliding():
-    assert True
